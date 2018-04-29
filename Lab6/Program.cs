753f94d8c49b7ae26d0c79edb4d47a48d6a08f01
Lovejoy-gitsit <26442317+Lovejoy-gitsit@users.noreply.github.com>
2018-04-29 17:07:43 -0400

    first commit for lab6

---
+++ Lab6/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        //putting random outside of Main so it can generate two random numbers that won't match all the time
+        public static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+
+
+            //asking user number of sides of die
+            int numberOfSides1 = GetNumber("Enter number of sides: ");
+
+            //if (!int.TryParse(Console.ReadLine() out, numberOfSides1))
+                
+            while (true)
+            {
+                //asking user if they want to continue playing
+                Console.WriteLine("Roll the dice? y/n");
+                string play;
+                play = Console.ReadLine();
+
+                if (play == "y")
+                {
+                    int dice1 = rnd.Next(1, numberOfSides1+1);
+                    
+                    int dice2 = rnd.Next(1, numberOfSides1+1);
+
+                    Console.WriteLine(dice1);
+                    Console.WriteLine(dice2);
+                    
+                }
+                else
+                {
+                    break;
+                }
+
+               
+            }
+
+        }
+
+        public static int GetNumber(string message)
+        {
+            Console.WriteLine(message);
+
+            return int.Parse(Console.ReadLine());
+            
+
+        }
+       
+    }
+}
